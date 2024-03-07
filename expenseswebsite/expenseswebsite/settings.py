f6e77fac079335c5bd7526867337f5c769f6c213
Naveen Ramanjulu <55444343+nram76@users.noreply.github.com>
2024-03-07 11:55:18 -0500

    resolved package issue with django-heroku install package for binary is django-on-heroku

--- expenseswebsite/expenseswebsite/settings.py
+++ expenseswebsite/expenseswebsite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,6 +124,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'expenseswebsite/static')]
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+
+django_on_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
